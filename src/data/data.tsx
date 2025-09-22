9252997c8ba78422ebe09bb3a0238d3d3a8fb7d0
MaxwellM <maxwellrmorin@gmail.com>
2025-09-22 14:16:08 -0600

    Getting closer to finishing the v1 of the homepage.

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -15,16 +15,22 @@ import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
 //import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
 import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
-import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
-import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
-import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
-import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
-import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
-import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
-import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
-import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
-import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
-import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
+import OrbitMessenger from '../images/portfolio/OrbitMessenger.png';
+import EggciteBike from '../images/portfolio/EggciteBike.png';
+import Eggman from '../images/portfolio/Eggman.png';
+import LostInSpace from '../images/portfolio/LostInSpace.png';
+import Salvare from '../images/portfolio/Salvare.png';
+import QuoteBuilder from '../images/portfolio/QuoteBuilder2.png';
+import ManufacturingPortal from '../images/portfolio/MfgPortal2.png';
+import ExecutiveDashboard from '../images/portfolio/Executive+Dashboard.png';
+import NIOSettings from '../images/portfolio/NIOSettings2.png';
+import MEDEVILFPS from '../images/portfolio/Medevil.png';
+import MEDEVILDialog from '../images/portfolio/MedevilDialog.png';
+import Algorithms from '../images/portfolio/ALGORITHMS.png';
+import Turtles from '../images/portfolio/Turtles.png';
+import NCBROWSE from '../images/portfolio/ncBrowse.png';
+import Scapegoat from '../images/portfolio/ScapegoatTree.png';
+import QuickCustomer from '../images/portfolio/QuickCustomer.png';
 import profilepic from '../images/AboutMePIC.jpg';
 import testimonialImage from '../images/testimonial.webp';
 import {
@@ -233,70 +239,106 @@ export const skills: SkillGroup[] = [
  */
 export const portfolioItems: PortfolioItem[] = [
   {
-    title: 'Project title 1',
-    description: 'Give a short description of your project here.',
+    title: 'ASGARD Portfolio Website',
+    description: 'Portfolio website created for the 521 Software Engineering Squadron to highlight the programs and porjects menat to be used as an advertising method to obtain new talent.',
     url: 'https://reactresume.com',
     image: porfolioImage1,
   },
   {
-    title: 'Project title 2',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage2,
+    title: 'Orbit Messenger',
+    description: 'Orbit Messenger is a multi-threaded, TLS encrypted, websocketed JavaFX application that allows multiple users to communicate via secured messaging with a Go server and PostgreSQL database.',
+    url: 'https://github.com/Orbit-Messenger',
+    image: OrbitMessenger,
   },
   {
-    title: 'Project title 3',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage3,
+    title: 'EggciteBike Game - Unity',
+    description: 'Eggcite Bike was created in Unity(V4.6). Every character and bicycle is powered by the use of Rigidbody2D, WheelJoint2D, HingeJoint2D, DistanceJoint2D, and SpringJoint2D components. The bicycles are powered through the WheelJoint2D components that rotate the wheel itself to create movement based upon user input. The goal is to reach the end of the level without crashing. Since you had no choice but to be an egg if you collide with anything you will break. You can rotate the bicycle by pressing the Q/E keys to help maintain balance.',
+    url: 'https://www.youtube.com/watch?v=swFlBIYBvLk',
+    image: EggciteBike,
   },
   {
-    title: 'Project title 4',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage4,
+    title: 'Eggman Game - Unity',
+    description: 'In my graduate level Advanced Unity Programming course I choose to do a 2D game. I did two projects this semester and this was my second one. This game was built in Unity(V4.6) and the images were created in Photoshop. This title demos Unitys Rigidbody 2D physics where two AI battle. The game showcases independent limb-to-limb movement instead of the traditional character controller approach. The logic the AI follows is built from three simple rules. If the enemy has a distance larger more than 25, move closer. If the enemy has a distance less than 12, move away. Upon being hit, the character will randomly choose between three attack styles, clockwise, counter clockwise, and random. Also there is a small random chance of jumping too, how fun?',
+    url: 'https://www.youtube.com/watch?v=GyfHQSEqoBI',
+    image: Eggman,
   },
   {
-    title: 'Project title 5',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage5,
+    title: 'Lost in Space Game - Unity',
+    description: 'I present to you, Lost in Space: Cosmonaut Edition. Formally known as Orbital Physics 2.0! A touch of satire can make learning fun! The game is currently in alpha stages (started 12 hours ago). All artwork/scripting/physics were created by me (and Sir Isaac Newton). This video is intended to demo a C# implementation of Newtonian Physics and a rough working main menu screen. More to follow, so stay tuned.',
+    url: 'https://www.youtube.com/watch?v=WqnNbWuZIdU',
+    image: LostInSpace,
   },
   {
-    title: 'Project title 6',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage6,
+    title: 'Salvare Game - Python3 & PyGame',
+    description: 'Group project creating a Python game using the PyGame SDL. The game is titled Salvare (Latin for savior) and is a 2D Newtonian Physics based orbit simulator. The objective of the game is to launch a probe from Earth and using a limited amount of fuel nudging it so that it will orbit other planets. The artwork was created using public domain images of construction paper and manipulated in Adobe Photoshop. This won second place at the 2012 UMaine Gaming Expo.',
+    url: 'https://www.youtube.com/watch?v=rJ9wC8DTqqg',
+    image: Salvare,
   },
   {
-    title: 'Project title 7',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage7,
+    title: 'Quote Builder',
+    description: 'This project was built with the intention to create "Quick Books" like suite where a particular company could create customized quotes, edit those quotes, and send those quotes to their respective partners. This software was built with JavaScript, AngularJS, GoLang, and PostgreSQL. Additionally, this software had access control through LDAP Authentication via Windows Server 2012 and auto generated reports.',
+    url: '',
+    image: QuoteBuilder,
   },
   {
-    title: 'Project title 8',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage8,
+    title: 'Manufacturing Hardware Testing Portal',
+    description: 'This project was the one stop site for all things related to the testing of all physical products a particular company. This software suite had the following abilities, initiate a multitude of tests, review the test results, run reports against test results, add/remove devices to test against, and contains 15+ single purpose tools that were intended to assist the manufacturing and business development team with common tasks related to these devices. This software was built with JavaScript, AngularJS, GoLang, and PostgreSQL. Additionally, this software had access control through LDAP Authentication via Windows Server 2012 and auto generated reports.',
+    url: '',
+    image: ManufacturingPortal,
   },
   {
-    title: 'Project title 9',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage9,
+    title: 'Executive Dashboard - Power BI',
+    description: 'Created an executive dashboard based off of our metrics from our ticketing system in Power BI Desktop. I configured Power BI Desktop to connect to our Dell K1000 Systems Management Appliance via a MySQL connection. Once configured I used the builtin tables and created calculated columns/measures  to show the following metrics: Closed Ticket Clients, Closed Ticket Types, Closed Tickets Quarter Over Quarter, All Tickets Closed By Category, Service Request Closed Tickets By Category, & Incident Closed Tickets By Category.',
+    url: '',
+    image: ExecutiveDashboard,
   },
   {
-    title: 'Project title 10',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage10,
+    title: 'Networked Input/Output Settings Page',
+    description: 'Networked Input/Output Settings page consists of all the changeable functionality within this particular device. The settings page comprises of four individual tabs that house similar functions/features. The page was redesigned with a modern styling while retaining an industrial look.',
+    url: '',
+    image: NIOSettings,
   },
   {
-    title: 'Project title 11',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage11,
+    title: 'MEDEVIL FPS Game - Unity',
+    description: 'Introductory Unity programming course where my final project for the class was to create a game. I choose to produce a 3D action game that featured a realistic medieval environment. The game featured a one square mile realistic environment, night-day system, enemy AI, and rag-doll physics. The game makes use of multiple assets such as: UFPS (Player/AI mechanics), UniStorm (weather), Medieval World Creation Kit (buildings), and the sound effects from Chivalry: Medieval Warfare.',
+    url: '',
+    image: MEDEVILFPS,
+  },
+  {
+    title: 'MEDEVIL Dialog Game - Unity',
+    description: 'In my Independent study (second Unity programming course) I produced a 3D game showcasing dialog within video games. My project consisted of five NPCs that offered missions and multiple dialog outcomes. The game makes use of multiple assets such as: UFPS (Player/AI mechanics), UniStorm (weather), Medieval World Creation Kit (buildings), and  Dialogue System (dialogue).',
+    url: '',
+    image: MEDEVILDialog,
+  },
+  {
+    title: 'Algorithms - Java',
+    description: 'In a group of four we were tasked with implementing three separate algorithms of various complexity in a language of our choosing. Our group was given Longest Common Substring, Satisfiability, and the Clique problem. We did solo implementations for Longest Common Substring, Satisfiablity, and worked together for the Clique problem. I choose Java as my language of choice for my two implementations.',
+    url: 'https://github.com/MaxwellM/DiscreteStructuresHW1',
+    image: Algorithms,
+  },
+  {
+    title: 'Turtles - Java',
+    description: 'In my object-oriented programming course I created a simulation with "turtles" that were set on a random path. Upon a turtle and turtle collision they would become sick, turn a darker color, and slow down for a certain length of time. If a turtle collided while being sick, each subsequent collision would increase the duration of its sickness also changing its color and speed. There was also a GUI window that displayed the current turtles direction and color. The simulation also included a counter for the infected turtles and a reset/pause feature.',
+    url: 'https://github.com/MaxwellM/JavaObjectOrientedProgrammingProjects',
+    image: Turtles,
+  },
+  {
+    title: 'NC BROWSE 2.0 - Java',
+    description: 'NCBrowse 2.0 is a Java based NetCDF visualizer that was initially developed by Donald W. Denbo employed by theNOAA/PMEL/EPIC group. I took this application and added further functionality such as a separate panel to display a ncDump, allowing up to eight variables be displayed even across all screens, and allowing simultaneous (multithreaded) videos. This project was intended to provide Phillip M. Dickens Supercomputing Lab at the University of Maine a useful application to display NetCDF files.',
+    url: 'https://github.com/MaxwellM/ncBrowse-revamp-CAPSTONE',
+    image: NCBROWSE,
+  },
+  {
+    title: 'Scapegoat Datastructures Tree - Java',
+    description: 'My visual Scapegoat Tree was built in Java using Swing Components and intended to be an educational tool for those learning databases, specifically Scapegoat Trees. The application supports adding, finding, or removing a node and producing an in-order, pre-order, or post-order traversal of the Scapegoat Tree. This was developed in a two person team consisting of Dylan Lougee and myself.',
+    url: 'https://github.com/MaxwellM/IntroToDataStructuresFinalProject',
+    image: Scapegoat,
+  },
+  {
+    title: 'Quick Customer Identification Tool - Java',
+    description: 'In my software engineering class we were divided into groups of nine and each assigned a few specific roles. The project was tasked with creating an application solution for a representative of Bangor Savings Bank that would be used by their help desk members taking incoming calls. The application would pull the phone number and run it against a SQL database and display relevant information. Our application was built in JavaFX with my primary role being UI I was tasked with implementing the frontend.',
+    url: '',
+    image: QuickCustomer,
   },
 ];
 
