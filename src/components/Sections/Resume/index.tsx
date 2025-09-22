a501e72aba3349cb981d1a7d45b8f8805c4e6be2
MaxwellM <maxwellrmorin@gmail.com>
2025-09-22 13:18:06 -0600

    Initial commit to PortfolioWebite3.0

--- src/components/Sections/Resume/index.tsx
+++ src/components/Sections/Resume/index.tsx
@@ -21,7 +21,23 @@ const Resume: FC = memo(() => {
           ))}
         </ResumeSection>
         <ResumeSection title="Skills">
-          <p className="pb-8">Here you can show a snapshot of your skills to show off to employers</p>
+          <p className="pb-8">
+            Product Management across multiple teams and products; Product Development from concept to customer; 
+            Effective Communication with various groups such as management, stakeholders, department teams, 
+            business partners, customers, and third-party integrators with custom and sensitive needs; Interpersonal 
+            Skills, extensive experience in presenting statistics and reports, coordinating multiple departments, 
+            and building relations with clients and shareholders. Inherited projects where client relationships were 
+            fragile. Consistently overcame these inherent challenges by personally interacting and hand delivering 
+            custom solutions to improve/repair relations; Teamwork, as both lead and follower; Web Design; Product 
+            Integration, coordinating interfaces between existing technologies to make them compatible, including 
+            with third party technologies; Networking Hardware, able to connect several devices to manage data 
+            collection and tasks for security, quality assurance and analysis; Managing Databases, able to create 
+            and utilize databases to document product performance, monitor current projects, and apply to future 
+            tasks; Planning, able to bring a product from concept to completion, as illustrated throughout entire 
+            work history by consistently noticing a need within the company and taking initiative to create products 
+            and solutions to fulfill that need; Computer Repair, repairing laptops and desktops at a component level. 
+            Collaboration, worked with multiple internal and external departments maintaining smooth product development
+          </p>
           <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
             {skills.map((skillgroup, index) => (
               <SkillGroup key={`${skillgroup.name}-${index}`} skillGroup={skillgroup} />
--- src/data/data.tsx
+++ src/data/data.tsx
@@ -9,10 +9,10 @@ import {
 } from '@heroicons/react/24/outline';
 
 import GithubIcon from '../components/Icon/GithubIcon';
-import InstagramIcon from '../components/Icon/InstagramIcon';
+//import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
 import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
-import TwitterIcon from '../components/Icon/TwitterIcon';
+//import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
 import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
 import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
@@ -25,7 +25,7 @@ import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
 import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
 import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
 import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
-import profilepic from '../images/profilepic.jpg';
+import profilepic from '../images/AboutMePIC.jpg';
 import testimonialImage from '../images/testimonial.webp';
 import {
   About,
@@ -44,7 +44,7 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
+  title: '../React Resume Template',
   description: "Example site built with Tim Baker's react resume template",
 };
 
@@ -69,24 +69,25 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `Maxwell Ross Morin.`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        I'm a Utah based <strong className="text-stone-100">Section Chief, Project Director, Product Owner, and Full Stack Software Engineer</strong>, currently working
+        at <strong className="text-stone-100">AFSC/SW based out of Hill, AFB</strong> helping build a modern, web-based softare, for the missileers
+        of our country.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        In my free time time, I'm often either <strong className="text-stone-100">tinkering on my vehicles</strong>,
+        reading <strong className="text-stone-100">Star Wars</strong> novels, or {' '}
+        <strong className="text-stone-100">cooking</strong> with my wife.
       </p>
     </>
   ),
   actions: [
     {
-      href: '/assets/resume.pdf',
+      //href: resumePDF,
+      href: '../assets/Resume.pdf',
       text: 'Resume',
       primary: true,
       Icon: ArrowDownTrayIcon,
@@ -104,16 +105,14 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
-  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
-  in.`,
+  description: `Interests include web development, cooking, and Star Wars. I have experience as a Section Chief, Project Director, Product Owner, Full-Stack Engineer, and IT Manager. Primarily my experience has derived from working for the Air Force building emergent software for the F-35, Sentinel Nuclear Missile Program,  Minuteman III Missile Program, and from a private company that builds compact radars for commercial and military use. Also worked as an IT consultant for the University of Maine's Psychology Department and on an IT Help Desk at a large construction company. For more info, check out my projects on GitHub.`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon},
+    {label: 'Location', text: 'West Haven, UT', Icon: MapIcon},
+    {label: 'Age', text: '37', Icon: CalendarIcon},
+    {label: 'Clearance', text: 'TS', Icon: FlagIcon},
+    {label: 'Interests', text: 'Tinkering, Coding, Cooking', Icon: SparklesIcon},
+    {label: 'Study', text: 'Univeristy of Maine', Icon: AcademicCapIcon},
+    {label: 'Employment', text: 'Unites States Air Foce', Icon: BuildingOffice2Icon},
   ],
 };
 
@@ -122,36 +121,23 @@ export const aboutData: About = {
  */
 export const skills: SkillGroup[] = [
   {
-    name: 'Spoken languages',
-    skills: [
-      {
-        name: 'English',
-        level: 10,
-      },
-      {
-        name: 'French',
-        level: 4,
-      },
-      {
-        name: 'Spanish',
-        level: 3,
-      },
-    ],
-  },
-  {
     name: 'Frontend development',
     skills: [
       {
-        name: 'React',
-        level: 9,
-      },
-      {
-        name: 'Typescript',
+        name: 'JavaScript',
         level: 7,
       },
       {
-        name: 'GraphQL',
+        name: 'AngularJS',
         level: 6,
+      },
+      {
+        name: 'React',
+        level: 4,
+      },
+      {
+        name: 'HTML/CSS',
+        level: 9,
       },
     ],
   },
@@ -159,33 +145,84 @@ export const skills: SkillGroup[] = [
     name: 'Backend development',
     skills: [
       {
-        name: 'Node.js',
-        level: 8,
-      },
-      {
-        name: 'Rust',
+        name: 'GoLang',
         level: 5,
       },
       {
-        name: 'Golang',
+        name: 'PostgreSQL',
+        level: 6,
+      },
+      {
+        name: 'Gin',
         level: 4,
       },
     ],
   },
   {
-    name: 'Mobile development',
+    name: 'Desktop Application Development',
     skills: [
       {
-        name: 'React Native',
+        name: 'Java',
+        level: 6,
+      },
+      {
+        name: 'C#',
+        level: 6,
+      },
+      {
+        name: 'Python',
+        level: 5,
+      },
+    ],
+  },
+  {
+    name: 'Business Tools',
+    skills: [
+      {
+        name: 'Jira',
         level: 9,
       },
       {
-        name: 'Flutter',
-        level: 4,
+        name: 'Pivotal Tracker',
+        level: 8,
       },
       {
-        name: 'Swift',
-        level: 3,
+        name: 'Microsoft Office',
+        level: 10,
+      },
+    ],
+  },
+  {
+    name: 'Microsoft Power App Suite',
+    skills: [
+      {
+        name: 'Power Apps',
+        level: 9,
+      },
+      {
+        name: 'Power Automate',
+        level: 9,
+      },
+      {
+        name: 'Power BI',
+        level: 7,
+      },
+    ],
+  },
+  {
+    name: 'Leadership',
+    skills: [
+      {
+        name: 'Employee Development',
+        level: 7,
+      },
+      {
+        name: 'Presentations',
+        level: 8,
+      },
+      {
+        name: 'Customer Relations',
+        level: 7,
       },
     ],
   },
@@ -268,39 +305,134 @@ export const portfolioItems: PortfolioItem[] = [
  */
 export const education: TimelineItem[] = [
   {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
+    date: 'May 2015',
+    location: 'University of Maine',
+    title: 'Bachelors in Computer Science',
     content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
   },
   {
-    date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
+    date: 'May 2012',
+    location: 'Eastern Maine Community College',
+    title: 'AA in Liberal Studies',
     content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
   },
 ];
 
 export const experience: TimelineItem[] = [
   {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
+    date: 'December 2022 - Present',
+    location: '517 Software Engineering Squadron',
+    title: 'Section Chief',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        Operate as a First Line Supervisor for three teams, spanning 14 individuals, as a Project Director, and 
+        working as an integrated civilian for our customers.
       </p>
     ),
   },
   {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
+    date: 'July 2022 - December 2022',
+    location: '521 Software Engineering Squadron',
+    title: 'Deputy Project Director',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        Operated as a second-in-command over a suite of applications named collectively as Torque that are tasked 
+        with aircraft maintenance/operations scheduling and personnel management. Torque consists of five separate 
+        product teams of around 40 individuals.
+      </p>
+    ),
+  },
+  {
+    date: 'July 2021 - July 2022',
+    location: '521 Software Engineering Squadron',
+    title: 'Lead Product Owner',
+    content: (
+      <p>
+        Lead and Owner of two software products, for the Torque project, that utilized modern web development languages, 
+        libraries, and frameworks, focusing on aircraft maintenance and operations scheduling. Managed a team of eight 
+        individuals through an Agile environment and the use of GitLab. Wrote user stories to describe new features, 
+        oversee the product's backlog, and validate what the Developers have delivered. Worked to keep the team unblocked 
+        and de-risked using hypothesis-driven development and are flexible and adaptable to a fast-changing environment. 
+        Led two software engineering products that included APIs, geospatial data management, and ML algorithms, as well 
+        as influenced teams to design, develop, test, deploy, maintain, and improve software. Wrote user stories, identify 
+        risks, manage schedules, releases, and communicate clearly with the product stakeholders. Assisted to solve the 
+        Nation's hardest problems surrounding enhancing the efficiency of schedule and unscheduled maintenance for over 
+        12 different air frames for the United States Air Force alongside people who shared my passion for changing the world.
+      </p>
+    ),
+  },
+  {
+    date: 'Feburary 2020 - July 2021',
+    location: '521 Software Engineering Squadron',
+    title: 'Product Owner',
+    content: (
+      <p>
+        Owner of four software products for the F35 through a partnership between the 309th SWEG and KesselRun. Managed a team 
+        of 14 individuals, across the four teams, through an Agile environment and the use of PivotalTracker. Wrote user stories 
+        to describe new features, oversee the product's backlog, and validate what the Developers have delivered. Worked to keep 
+        the team unblocked and de-risked using hypothesis-driven development and are flexible and adaptable to a fast-changing 
+        environment. Led four software engineering products that included APIs, geospatial data management, and ML algorithms, 
+        as well as influenced teams to design, develop, test, deploy, maintain, and improve software. Wrote user stories, 
+        identify risks, manage schedules, releases, and communicate clearly with the product stakeholders. Assisted to solve 
+        the Nation's hardest problems for the United States Air Force alongside people who shared my passion for changing the world.
+      </p>
+    ),
+  },
+  {
+    date: 'Feburary 2020 - July 2021',
+    location: '521 Software Engineering Squadron',
+    title: 'Product Owner',
+    content: (
+      <p>
+        Owner of four software products for the F35 through a partnership between the 309th SWEG and KesselRun. Managed a team 
+        of 14 individuals, across the four teams, through an Agile environment and the use of PivotalTracker. Wrote user stories 
+        to describe new features, oversee the product's backlog, and validate what the Developers have delivered. Worked to keep 
+        the team unblocked and de-risked using hypothesis-driven development and are flexible and adaptable to a fast-changing 
+        environment. Led four software engineering products that included APIs, geospatial data management, and ML algorithms, 
+        as well as influenced teams to design, develop, test, deploy, maintain, and improve software. Wrote user stories, 
+        identify risks, manage schedules, releases, and communicate clearly with the product stakeholders. Assisted to solve 
+        the Nation's hardest problems for the United States Air Force alongside people who shared my passion for changing the world.
+      </p>
+    ),
+  },
+  {
+    date: 'August 2019 - Feburary 2020',
+    location: 'LogMet',
+    title: 'Electronics Engineer Contractor',
+    content: (
+      <p>
+        (D) Level Maintenance and related tasks for the 309th Electronics Maintenance Group (EMXG), Ogden Air Logistics Complex 
+        (OO-ALC) at Hill Air Force Base (AFB), Utah (UT). Main duties were to troubleshoot faulty mechanic instruments from both 
+        F-16 Fighting Falcons and C-130 Hercules aircraft. Tasks involved diagnosing electrical and electronic malfunctions within 
+        systems like gauges, sensors, and control panels. Used specialized testing equipment to pinpoint the root cause of the problem, 
+        utilizing Technical Orders (schematics and technical manuals) for guidance. Once identified, repaired or replaced defective 
+        components such as wiring, circuit boards, or sensors, ensuring proper functionality. After repair, calibrated and test the 
+        instruments to guarantee accuracy and reliability under flight conditions. Finally, document all repairs performed, maintaining 
+        a detailed record of the work completed for future reference and quality control.
+      </p>
+    ),
+  },
+  {
+    date: 'Feburary 2017 - June 2019',
+    location: 'SpotterRF',
+    title: 'Product Owner, Full-Stack Dev, & IT Manager',
+    content: (
+      <p>
+        Owner of two internal software products: manufacturing and web business development. Designed web sites with features like 
+        Active Directory Lightweight Directory Access Protocol (LDAP) Authentication, quality assurance tools, saving statistics and 
+        snapshots to databases for tested and released products. Served as an interdepartmental liaison. Worked as an intermediary 
+        between manufacturing and business development departments in bringing understanding of product vision to development teams. 
+        Facilitated meetings between our company and our partners via video conferencing. Met with manufacturing and business development 
+        teams whiteboarding and implementing solutions to feature requests. Conceptualized and presented a new product design for 
+        manufacturing software, worked with manufacturing and management to execute product and bring on all software from a non-secured 
+        service to an LDAP secured service. Met daily with the manufacturing team and coordinated their needs/priorities and made 
+        adjustments accordingly to my manufacturing software. Conceptualized and developed user facing quote building web service. 
+        This product is used daily by customers and has tools for our sales team to help accelerate generating business and run reports, 
+        providing valuable business insights. Interacted daily with critical third-party integrators, all with custom and sensitive 
+        needs and tight deadlines. Presented to management monthly about product development status. Worked with the manufacturing 
+        and business development teams coordinating viable solutions in a timely manner. Networked our building’s long term security 
+        monitoring system. Researched, Purchased, Installed, and Maintained database servers for data collection in manufacturing 
+        utilizing PostgreSQL.
       </p>
     ),
   },
@@ -340,23 +472,23 @@ export const contact: ContactSection = {
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
+      text: 'maxwellrmorin@gmail.com',
+      href: 'mailto:maxwellrmorin@gmail.com',
     },
     {
       type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
+      text: 'West Haven UT, USA',
+      href: 'https://maps.app.goo.gl/RaEM6yqwvyDxFpB89',
     },
-    {
-      type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
-    },
+    //{
+    //  type: ContactType.Instagram,
+    //  text: '@tbakerx',
+    //  href: 'https://www.instagram.com/tbakerx/',
+    //},
     {
       type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      text: 'MaxwellM',
+      href: 'https://github.com/MaxwellM/',
     },
   ],
 };
@@ -365,9 +497,9 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
-  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
-  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/reactresume/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/MaxwellM/'},
+  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/1742163/maxwell-morin'},
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/maxwell-morin/'},
+  //{label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/reactresume/'},
+  //{label: 'X', Icon: TwitterIcon, href: 'https://x.com/MAXintosh2010'},
 ];
